4a51d47381e98862cd322e3e68b124e10d3bdd62
HOHAHOHI <160473938+HOHAHOHI@users.noreply.github.com>
2024-06-25 14:35:00 +0900

    choijh

--- amado/src/main/java/com/spring/app/amado/controller/ControllerJH.java
+++ amado/src/main/java/com/spring/app/amado/controller/ControllerJH.java
@@ -1,9 +1,19 @@
 package com.spring.app.amado.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class ControllerJH {
-	//안녕하세요~
 	
+	
+	@GetMapping("/saleview/sale.do")
+	public ModelAndView index(ModelAndView mav) {
+
+		mav.setViewName("saleview/sale.tiles2");
+		// /WEB-INF/views/tiles2/main/index.jsp
+		
+		return mav;
+	}	
 }
