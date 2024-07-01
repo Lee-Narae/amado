2a391d93a2fdc59780bd4c8271e081eceaf6f4d6
leejamong2 <leejamong2@naver.com>
2024-07-01 12:28:16 +0900

    플리마켓 부이오 만듬

--- amado/src/main/java/com/spring/app/amado/controller/ControllerJY.java
+++ amado/src/main/java/com/spring/app/amado/controller/ControllerJY.java
@@ -1,8 +1,16 @@
 package com.spring.app.amado.controller;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.spring.app.common.MyUtil;
 
 @Controller
 public class ControllerJY {
@@ -11,9 +19,13 @@ public class ControllerJY {
 	//dkdkkkk지윤이다
 	///////////////////////////////////////////////////////////////////////////
 	@RequestMapping(value="/club/oldshop.do")
-	public ModelAndView oldshop(ModelAndView mav) {
+	public ModelAndView oldshop(ModelAndView mav) { //중고상품 리스트 보요주기
+
+
+		
 		mav.setViewName("club/oldshop.tiles2");
 		//    /WEB-INF/views/club/oldshop.jsp
+		
 		return mav;
 		
 	}
